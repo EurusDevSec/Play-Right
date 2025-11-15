a2adbab2e42e5f1e0f988569faf6795d1097d672
LeganSar44 <hoangslevan@gmail.com>
2025-11-15 22:33:44 +0700

    feat: add initial implementation for VnExpress data crawling and display

---
+++ superMiniArchitecture/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+st.title("Crawl du lieu VnExpress")
+
+df = pd.read_csv('vnexpress_data.csv')
+st.subheader(f"hien thi {len(df)} bai viet da crawl: ")
+for index, row in df.iterrows():
+    with st.container(border=True):
+        st.subheader(row['TieuDe'])
+        st.write(f"[Doc bai viet]({row['DuongDan']})")
