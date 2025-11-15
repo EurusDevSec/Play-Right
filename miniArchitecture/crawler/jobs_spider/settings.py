4edb8ccaacdea31acf1305c3914554f4b31ac142
LeganSar44 <hoangslevan@gmail.com>
2025-11-15 20:52:04 +0700

    Remove tracked HTML files and other deletions

--- miniArchitecture/crawler/jobs_spider/settings.py
+++ miniArchitecture/crawler/jobs_spider/settings.py
@@ -1,16 +1,20 @@
+BOT_NAME = "jobs_spider"
+SPIDER_MODULES = ["jobs_spider.spiders"]
+NEWSPIDER_MODULE = "jobs_spider.spiders"
+
 DOWNLOADER_MIDDLEWARES = {
-    'scrapy_playwright.middleware.ScrapyPlaywrightDownloadHandler': 543,
+    "scrapy_playwright.middleware.ScrapyPlaywrightDownloadHandler": 543,
 }
-TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
-PLAYWRIGHT_BROWSER_TYPE = 'chromium'
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+PLAYWRIGHT_BROWSER_TYPE = "chromium"
 DOWNLOAD_HANDLERS = {
-    'http': 'scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler',
-    'https': 'scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler',
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
 ROBOTSTXT_OBEY = True
 DOWNLOAD_DELAY = 1  # adjust according to target
 CONCURRENT_REQUESTS = 8
 
 ITEM_PIPELINES = {
-   'jobs_spider.pipelines.SQLitePipeline': 300,
+    "jobs_spider.pipelines.SQLitePipeline": 300,
 }
