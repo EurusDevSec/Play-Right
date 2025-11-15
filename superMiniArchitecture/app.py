3819e1267243cedf60ca1cc5fd9a3339971403ca
LeganSar44 <hoangslevan@gmail.com>
2025-11-15 22:50:59 +0700

    refactor: restructure article display layout in VnExpress crawler

--- superMiniArchitecture/app.py
+++ superMiniArchitecture/app.py
@@ -6,11 +6,18 @@ st.title("Crawl du lieu VnExpress")
 
 df = pd.read_csv('vnexpress_data.csv')
 st.subheader(f"hien thi {len(df)} bai viet da crawl: ")
+N_COLS=3
+cols = st.columns(N_COLS)
+
 for index, row in df.iterrows():
 
-
-    with st.container(border=True):
-        if pd.notna(row['HinhAnh']) and row['HinhAnh'] != "":
-            st.image(row['HinhAnh'])
-        st.subheader(row['TieuDe'])
-        st.write(f"[Doc bai viet]({row['DuongDan']})")
+    col_index = index % N_COLS
+    with cols[col_index]:
+# tao card cho tung bai
+        with st.container(border=True): 
+            if pd.notna(row['HinhAnh']) and row['HinhAnh'] != "":
+                st.image(row['HinhAnh'], use_column_width=True) 
+            
+          
+            st.markdown(f"**{row['TieuDe']}**") 
+            st.write(f"[Đọc bài viết]({row['DuongDan']})")
